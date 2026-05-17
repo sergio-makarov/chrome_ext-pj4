c75ea4228551c749eaafdad81913be2859200082
Sergio Makarov <aranya12062007@gmail.com>
2026-05-17 21:21:06 +0530

    js localStorage fix and update scroll

--- script.js
+++ script.js
@@ -3,44 +3,57 @@ const inputEl = document.getElementById('input-el');
 const saveInputBtn = document.getElementById('save_input-btn');
 const clearInputsBtn = document.getElementById('clear_inputs-btn');
 const ulEl = document.getElementById('ul-el');
+const ulContainer = document.querySelector('.web_link-list');
 
 
 //all let elements
-let urlArray = JSON.parse(localStorage.getItem('urlLinks')) || [];
-displayWebLink();
+let urlArray;
+
+
+
+function init() {
+  urlArray = JSON.parse(localStorage.getItem('urlLinks')) || [];
+  displayWebLink(urlArray);
+}
 
 inputEl.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
-    addLinkToArray();
+    addLinkToArray(urlArray);
   }
 })
 
 
 
 //All events..
-saveInputBtn.addEventListener('click', () => {addLinkToArray()});
-clearInputsBtn.addEventListener('click',()=>{clearArray()});
+saveInputBtn.addEventListener('click', () => { addLinkToArray(urlArray) });
+clearInputsBtn.addEventListener('click', () => { clearArray(urlArray) });
 
 
 
 
-function addLinkToArray() {
-  urlArray.push(inputEl.value);
-  localStorage.setItem('urlLinks',JSON.stringify(urlArray));
-  displayWebLink();
+function addLinkToArray(nameArray) {
+  nameArray.push(inputEl.value);
+  localStorage.setItem('urlLinks', JSON.stringify(nameArray));
+  displayWebLink(nameArray);
   inputEl.value = '';
+  ulContainer.scrollTop = ulContainer.scrollHeight;
 }
 
 
-function displayWebLink(){
+function displayWebLink(nameArray) {
   ulEl.innerHTML = '';
-  urlArray.forEach((urlLink)=>{
-    ulEl.innerHTML +=`<li><a href="${urlLink}">${urlLink}</a></li>`;
+  nameArray.forEach((urlLink) => {
+    ulEl.innerHTML += `<li><a href="${urlLink}">${urlLink}</a></li>`;
   });
 }
 
-function clearArray(){
-  urlArray = [];
-  localStorage.setItem('urlLinks',JSON.stringify(urlArray));
-  displayWebLink();
+function clearArray(nameArray) {
+  nameArray = [];
+  localStorage.setItem('urlLinks', JSON.stringify(nameArray));
+  init();
 }
+
+
+
+// load the logic....
+document.addEventListener('DOMContentLoaded',()=>{init()});
