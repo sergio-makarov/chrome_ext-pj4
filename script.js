8c643e588749532aa1cd366d18f2c5ce7096c24b
Sergio Makarov <aranya12062007@gmail.com>
2026-05-17 18:22:04 +0530

    add localStorage

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 //all const elements
 const inputEl = document.getElementById('input-el');
 const saveInputBtn = document.getElementById('save_input-btn');
+const clearInputsBtn = document.getElementById('clear_inputs-btn');
 const ulEl = document.getElementById('ul-el');
 
 
 //all let elements
-let urlArray = [];
-
+let urlArray = JSON.parse(localStorage.getItem('urlLinks')) || [];
+displayWebLink();
 
 inputEl.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
     addLinkToArray();
   }
 })
-saveInputBtn.addEventListener('click', () => {
-  addLinkToArray();
-});
+
+
+
+//All events..
+saveInputBtn.addEventListener('click', () => {addLinkToArray()});
+clearInputsBtn.addEventListener('click',()=>{clearArray()});
 
 
 
 
 function addLinkToArray() {
   urlArray.push(inputEl.value);
+  localStorage.setItem('urlLinks',JSON.stringify(urlArray));
   displayWebLink();
   inputEl.value = '';
 }
@@ -32,4 +37,10 @@ function displayWebLink(){
   urlArray.forEach((urlLink)=>{
     ulEl.innerHTML +=`<li><a href="${urlLink}">${urlLink}</a></li>`;
   });
+}
+
+function clearArray(){
+  urlArray = [];
+  localStorage.setItem('urlLinks',JSON.stringify(urlArray));
+  displayWebLink();
 }
